4090a8dd6890bf749c6c527ff732f0672156ae22
Sunny14578 <59717550+Sunny14578@users.noreply.github.com>
2023-03-14 15:27:18 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 34120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+m = int(input())
+n = int(input())
+
+arr = [[] for i in range(m+1)]
+
+for i in range(n):
+    i, v = map(int, input().split(' '))
+    arr[i].append(v)
+    arr[v].append(i)
+
+visit = [0]*(m+1)
+
+def bfs(x):
+    Q = deque()
+    Q.append(x)
+    
+    while Q:
+        x = Q.popleft()
+        
+        for i in arr[x]:
+            if visit[i] == 0:
+                visit[i] = 1
+                Q.append(i)
+                
+    return visit
+                
+print(sum(bfs(1))-1)
