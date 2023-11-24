3076780e710a1df0a995b023a7f690b0f240e726
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2023-11-24 10:52:24 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,47 +1,54 @@
 def solution(survey, choices):
     
-    p = {'R' : 0, 'T' : 0,
-        'C' : 0, 'F' : 0,
-        'J' : 0, 'M' : 0,
-        'A' : 0, 'N' : 0}
+#     p = {'R' : 0, 'T' : 0,
+#         'C' : 0, 'F' : 0,
+#         'J' : 0, 'M' : 0,
+#         'A' : 0, 'N' : 0}
     
-    for i in range(len(survey)):
-        s_list = list(survey[i])
-        print(s_list)
+#     for i in range(len(survey)):
+#         s_list = list(survey[i])
+#         print(s_list)
         
-        if choices[i] > 4:
-            p[s_list[1]] += choices[i]-4
-        elif choices[i] < 4:
-            p[s_list[0]] += 4-choices[i]
+#         if choices[i] > 4:
+#             p[s_list[1]] += choices[i]-4
+#         elif choices[i] < 4:
+#             p[s_list[0]] += 4-choices[i]
+    score = [3,2,1,0,1,2,3]
+    dict = {'R':0,'T':0,'C':0,'F':0,'J':0,'M':0,'A':0,'N':0}
+    for i,choice in enumerate(choices):
+        if choice <= 3:
+            dict[survey[i][0]] += score[choice-1]
+        elif choice >= 5:
+            dict[survey[i][1]] += score[choice-1]
             
-    answer = ''        
-            
-    if p['R'] >= p['T']:
+    answer = ''
+    if dict['R'] >= dict['T']:
         answer += 'R'
     else:
-        answer += 'T' 
-   
-    if p['C'] >= p['F']:
-        answer += 'C' 
+        answer += 'T'
+    if dict['C'] >= dict['F']:
+        answer += 'C'
     else:
-        answer += 'F' 
-        
-    if p['J'] >= p['M']:
-        answer += 'J' 
+        answer += 'F'
+    if dict['J'] >= dict['M']:
+        answer += 'J'
     else:
-        answer += 'M' 
-        
-    if p['A'] >= p['N']:
-        answer += 'A' 
+        answer += 'M'
+    if dict['A'] >= dict['N']:
+        answer += 'A'
     else:
-        answer += 'M' 
+        answer += 'N'
         
-    li = list(p.items())
+        
+#     li = list(p.items())
+#     print(li)
     
-    f= ''
-    
-    for i in range(0,8,2):
-        if li[i][1] < li[i+1][1]: f += li[i+1][0]
-        else:   f += li[i][0]
+#     for i in range(0,8,2):
+#         if li[i][1] < li[i+1][1]: 
+#             answer += li[i+1][0]
+#         else:   
+#             answer += li[i][0]
+            
+#     print(answer)
         
-    return f
+    return answer
