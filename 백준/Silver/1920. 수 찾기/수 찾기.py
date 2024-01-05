0c1905830e934053f4c7ce6875d707bde11a4e82
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-05 11:11:52 +0900

    [Silver IV] Title: 수 찾기, Time: 4648 ms, Memory: 145708 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(set(map(int, input().split())))
+m = int(input())
+find = list(map(int, input().split()))
+
+dic = {}
+
+for i in find:
+    if i not in dic:
+        if i in a:
+            dic[i] = 1
+        else:
+            dic[i] = 0
+    print(dic[i])
