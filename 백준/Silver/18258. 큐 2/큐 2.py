b2386045069246392dc34bc608c40d61baec479a
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-06 16:20:44 +0900

    [Silver IV] Title: 큐 2, Time: 944 ms, Memory: 254504 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,30 +1,30 @@
 import sys
 from collections import deque
 n = int(input())
-queue = deque([])
+queue = deque()
 for i in range(n):
     com = sys.stdin.readline().split()
     if com[0] == 'push':
         queue.append(com[1])
     elif com[0] == 'pop':
-        if len(queue) == 0:
-            print(-1)
-        else:
+        if queue:
             print(queue.popleft())
+        else:
+            print(-1)
     elif com[0] == 'size':
         print(len(queue))
     elif com[0] == 'empty':
-        if len(queue) == 0:
-            print(1)
-        else:
+        if queue:
             print(0)
+        else:
+            print(1)
     elif com[0] == 'front':
         if len(queue) == 0:
             print(-1)
         else:
             print(queue[0])
     elif com[0] == 'back':
-        if len(queue) == 0:
-            print(-1)
-        else:
+        if queue:
             print(queue[-1])
+        else:
+            print(-1)            
