619f0cac08bdbbe0a36b5d3dd4a84cc7abdfc439
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-10 02:20:18 +0900

    [Silver IV] Title: 스택, Time: 152 ms, Memory: 111856 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+
+for i in range(n):
+    com = sys.stdin.readline().split()
+    
+    if com[0] == 'push':
+        stack.append(com[1])
+    elif com[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif com[0] == 'size':
+        print(len(stack))
+    elif com[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif com[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
