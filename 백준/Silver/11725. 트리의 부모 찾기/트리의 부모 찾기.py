4137f4b64a2370f2209d3f7c0537991ffcd3ed9d
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-12 14:23:03 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 4144 ms, Memory: 49764 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,28 +1,27 @@
 from collections import deque
 
+
 n = int(input())
 
-arr = [[]*i for i in range(n+1)]
+graph = [[] for _ in range(n+1)]
 
-for i in range(1, n):
-    a, b = map(int, input().split(' '))
-    arr[a].append(b)
-    arr[b].append(a)
-
+for i in range(n-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
 visit = [0]*(n+1)
 
 def bfs():
-    q = deque()
-    q.append(1)
+    q = deque([1])
     
     while q:
-        l = q.popleft()
-        
-        for i in arr[l]:
+        val = q.popleft()
+ 
+        for i in graph[val]:
             if visit[i] == 0:
+                visit[i] = val
                 q.append(i)
-                visit[i] = l
-                
 bfs()
 
 for i in range(2, len(visit)):
