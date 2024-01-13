df259bca555dc8c0929b0a3bc6db18dc395240aa
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-13 17:25:28 +0900

    [Gold IV] Title: 세부, Time: 856 ms, Memory: 85784 KB -BaekjoonHub

--- 백준/Gold/13905. 세부/세부.py
+++ 백준/Gold/13905. 세부/세부.py
@@ -1,46 +1,36 @@
-import sys
-input=sys.stdin.readline
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
 
-def Find(x):
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 
-    if x!=disjoint[x]:
-        disjoint[x]=Find(disjoint[x])
-    return disjoint[x]
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return
+        self.parent[root_x] = root_y
 
-N,M=map(int,input().split())
+if __name__ == '__main__':
+    from sys import stdin, setrecursionlimit
+    input = stdin.readline
+    setrecursionlimit(10 ** 7)
 
-S,E=map(int,input().split())
-
-disjoint=[ _ for _ in range(N+1) ]
-
-edge=[]
-
-for i in range(M):
-
-    a,b,c=map(int,input().split())
-
-    edge.append((c,a,b))
-
-edge.sort(key=lambda x:-x[0])
-
-check=[]
-for value,x,y in edge:
-
-
-    x=Find(x)
-    y=Find(y)
-
-
-    if x!=y:
-        if x>y:
-            disjoint[x]=y
-        else:
-            disjoint[y]=x
-
-    if Find(S)==Find(E):
-        check.append(value)
-
-if check:
-    print(max(check))
-else:
-    print(0)
+    n, m = map(int, input().split())
+    s, e = map(int, input().split())
+    bridge = []
+    for _ in range(m):
+        x, y, k = map(int, input().split())
+        bridge.append((k, x, y))
+    bridge.sort()
+    djs = DisjointSet(n)
+    last = 0
+    while djs.find(s) != djs.find(e) and bridge:
+        d, x, y = bridge.pop()
+        last = d
+        djs.union(x, y)
+    print(last if djs.find(s) == djs.find(e) else 0)
