60376da610d6f08721a02cb3689c8ec436b2562d
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-15 17:04:41 +0900

    [Gold III] Title: 아기 상어, Time: 196 ms, Memory: 114676 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,71 +1,72 @@
 from collections import deque
 
-n = int(input())
+# 가장 가까운 곳의 먹이가 있는 곳으로 이동한다 (BFS 탐색 활용)
+# 너비 깊이만큼이 t초 지났다고 판단
 
+n = int(input())
 arr = []
 start = 0
-
 for i in range(n):
-    c = list(map(int, input().split(' ')))
+    li = list(map(int, input().split()))
     
-    if 9 in c:
-        start = [i, c.index(9)]
-        
-    arr.append(c)
-
-# 상 좌 우 하
-dx = [-1, 0, 0, 1]
-dy = [0, -1, 1, 0]
-
-shark = 2
-time = 0
+    if 9 in li:
+        start = [i, li.index(9)]
+    arr.append(li)
+    
+size = 2
 cnt = 0
+time = 0
 
-def bfs(x, y):
-    global shark, cnt
+dx = [-1, 1, 0, 0] # 상 하 좌 우
+dy = [0, 0, -1, 1]
+
+def bfs(x, y): 
+    global cnt, size
     
-    visit = [[0]*n for i in range(n)]
+    visit = [[0] * n for _ in range(n)]
     
-    l1 = [] 
+    l1 = []
+    arr[x][y] = 0 # 상어의 출발위치를 다시 방문할수도 있으므로 0으로 만든다.
     q = deque()
     q.append([x, y])
-    arr[x][y] = 0
     visit[x][y] = 1
     
     while q:
         x, y = q.popleft()
         
-        if cnt == shark:
+        # 물고기를 먹은수(cnt)와 상어의크기(size)가 같을경우 
+        # 상어의 크기를 1늘리고 물고기는 0으로한다.
+        if cnt == size: 
+            size += 1
             cnt = 0
-            shark += 1
-
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0 and arr[nx][ny] <= shark:
-                
-                if arr[nx][ny] == 0 or arr[nx][ny] == shark:
+            # 물고기의 크기가 상어보다 같거나 작아야 이동할수 있으므로 조건 추가
+            if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] <= size and visit[nx][ny] == 0:
+                # 물고기의 크기가 같을때는 이동만 할 수 있다.
+                if arr[nx][ny] == 0 or arr[nx][ny] == size:
                     q.append([nx, ny])
-                    visit[nx][ny] = visit[x][y]+1
-                    
-                elif arr[nx][ny] < shark:
+                    visit[nx][ny] = visit[x][y] + 1 # 방문할때 마다 1씩 증가시켜서 시간으로 이용한다.
+                # 물고기의 크기가 상어보다 작은 경우에 리스트에 해당 (시간, x좌표, y좌표를 추가한다.)
+                elif arr[nx][ny] < size:
                     l1.append([visit[x][y], nx, ny])
-                    
-           # print(l1)
-            
-    return sorted(l1, key = lambda x: (x[0], x[1], x[2]))
+    
+    # 시간을 기준으로 오름차순 정렬하여 가장 가까운 위치를 리턴하게끔한다
+    return sorted(l1, key = lambda x: (x[0], x[1], x[2])) 
 
 x, y = start
 
 while True:
-
-    l = deque(bfs(x, y))
+    l1 = deque(bfs(x, y))
     
-    if not l:
+    if not l1:
         break
-    
-    t, x, y = l.popleft()
+        
+    t, x, y = l1.popleft()
     cnt += 1
     time += t
+    
 print(time)
