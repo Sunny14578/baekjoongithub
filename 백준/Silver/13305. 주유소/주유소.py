3ae4b200f051c263c17d154a39cd1661193eb0eb
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-18 16:24:56 +0900

    [Silver III] Title: 주유소, Time: 168 ms, Memory: 45404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,17 +1,21 @@
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
-dis = list(map(int, input().split()))
-price = list(map(int, input().split()))
 
-MIN = price[0]
+l1 = list(map(int, input().split(' ')))
+l2 = list(map(int, input().split(' ')))
+
+l3 = l2.copy()
+l3.sort()
+l1.append(0)
+
 money = 0
+mini = l2[0]
+cnt = 0
 
-for i in range(len(dis)):
-    if MIN > price[i]:
-        MIN = price[i]
+for i in range(0, len(l2)):
+    if mini > l2[i]:
+        mini = l2[i]
     
-    money += MIN * dis[i]
+    money += (mini * l1[cnt])
+    cnt+=1
+        
 print(money)
