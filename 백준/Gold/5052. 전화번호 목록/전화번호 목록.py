b669893186b0bfd63a66c9c894ac753ef458d41a
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-01-18 19:55:53 +0900

    [Gold IV] Title: 전화번호 목록, Time: 176 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    phone = []
+    for _ in range(n):
+        phone.append(input().rstrip())
+    
+    phone.sort()
+    
+    check = True
+    
+    for i in range(n-1):
+        long = len(phone[i])
+        if phone[i] == phone[i+1][:long]:
+            check = False
+            break
+    if check:
+        print('YES')
+    else:
+        print('NO')
