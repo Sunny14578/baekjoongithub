a57d99577299e664890d5d37dd6184c52fb8e9a6
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-03-28 09:24:43 +0900

    [Silver I] Title: 안전 영역, Time: 844 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,22 +1,23 @@
 from collections import deque
-import copy
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())
 
-arr = []
+graph = []
 MAX = 0
-for i in range(n):
-    c = list(map(int, input().split(' ')))
-    arr.append(c)
-    
-    if max(c) >= MAX:
-        MAX = max(c)
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+for i in range(n):
+    l = list(map(int, input().split()))
+    MAX = max(max(l), MAX)
+    
+    graph.append(l)
+    
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 def bfs(x, y):
-    
     q = deque()
     q.append([x, y])
     visit[x][y] = 1
@@ -29,27 +30,26 @@ def bfs(x, y):
             ny = y + dy[i]
             
             if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0:
-                q.append([nx, ny])
                 visit[nx][ny] = 1
-
-cnt = 1
+                q.append([nx, ny])
+                
+answer = 1
 
 for d in range(1, MAX+1):
-    graph = copy.deepcopy(arr)
-    visit = [[0]*n for i in range(n)]
+    visit = [[0]  *n for i in range(n)]
     
     for i in range(n):
         for j in range(n):
             if graph[i][j] <= d:
                 visit[i][j] = 1
-    cnt2 = 0
+    cnt = 0
     
     for i in range(n):
         for j in range(n):
             if visit[i][j] == 0:
                 bfs(i, j)
-                cnt2 += 1
-                
-    cnt = max(cnt, cnt2)
-
-print(cnt)                
+                cnt += 1
+    
+    answer = max(cnt, answer)
+    
+print(answer)
