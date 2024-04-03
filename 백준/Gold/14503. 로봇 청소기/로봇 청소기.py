fedb9c189bd49534b96d7b1a054f53a5f83dda26
Choi taeyang <59717550+Sunny14578@users.noreply.github.com>
2024-04-04 02:09:27 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,34 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-graph = []
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-
 n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+x, y, d = map(int, input().split())
 
-graph[r][c] = -1
-count = 1
-while graph[r][c] != 1:
-    temp = False
+graph = []
+
+for _ in range(n):
+    v = list(map(int, input().split()))
+    graph.append(v)
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]      
+
+cnt = 1
+graph[x][y] = -1
+    
+while graph[x][y] != 1:
+    flag = 1
+        
     for _ in range(4):
         d -= 1
+        
         if d == -1:
             d = 3
-        nr = r + dr[d]
-        nc = c + dc[d]
-        if graph[nr][nc] == 0:
-            r = nr
-            c = nc
-            graph[r][c] = -1
-            count += 1
-            temp = True
-            break
-    if not temp:
-        r += dr[d-2]
-        c += dc[d-2]
         
-print(count)
+        nx = x + dx[d]   
+        ny = y + dy[d]
+ 
+        if graph[nx][ny] == 0:
+            x, y = nx, ny
+            graph[x][y] = -1
+            cnt += 1
+            flag = 0
+            break
+                    
+    if flag == 1:
+        x += dx[d-2]
+        y += dy[d-2]
+        
+print(cnt)
